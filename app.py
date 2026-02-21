a5e7a7879b634415e404da424e78cbb4484415db
prokimduc-dotcom <prokimduc@gmail.com>
2026-02-21 22:53:46 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import sympy as sp
+
+app = Flask(__name__)
+
+@app.route("/calc")
+def calculate():
+    expr = request.args.get("expr")
+    if not expr:
+        return jsonify({"error": "Missing expression"})
+    
+    try:
+        x = sp.symbols('x')
+        result = sp.simplify(expr)
+        return jsonify({"result": str(result)})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run()
